290aa9e9171bd3e641bf7867b66bd4e6e86650d5
nishantak09 <nishantak09@gmail.com>
2024-01-28 18:22:03 +0530

    ok looking

--- website/templates/script/script.js
+++ website/templates/script/script.js
@@ -14,5 +14,17 @@ $(document).ready(function(){
         $("#commodity").attr("src", imgs[currIn]);
     }
 
+    $("#login-form").submit(function(event) {
+        event.preventDefault();
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        if (email === "" || password === "") {
+            $("#form-message").text("Please fill in all fields.");
+            return;
+        }
+        // Login logic
+    });
     
 });
